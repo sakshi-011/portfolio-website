f072b538012b278cf7952c6516a3d75fbc4ee6e7
sakshi-011 <101360946+sakshi-011@users.noreply.github.com>
2022-06-24 23:55:46 +0530

    Initial Commit

---
+++ Frontend/src/js/controller.js
@@ -0,0 +1,72 @@
+'use strict';
+
+import mainView from './Views/mainView.js';
+import { bioArray } from './model.js';
+import contact from './contact.js';
+import { FORM_URL } from './config.js';
+
+const form = document.querySelector('.contact--form');
+
+const controlHandlerBio = function () {
+  mainView.renderBio(bioArray);
+};
+
+const controlHandlerForm = async function (e) {
+  if (!contact.validateFormFields()) return;
+  let message = new FormData(e.target);
+
+  const status = document.getElementById('form--status');
+
+  try {
+    const response = await fetch(FORM_URL, {
+      method: form.method,
+      body: message,
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      status.innerHTML = 'Thanks for your submission!';
+      form.reset();
+    } else {
+      const data = await response.json();
+
+      if (Object.hasOwn(data, 'errors')) {
+        status.innerHTML = data['errors']
+          .map(error => error['message'])
+          .join(', ');
+      } else {
+        status.innerHTML = 'Oops! There was a problem submitting your form';
+      }
+    }
+  } catch (error) {
+    status.innerHTML = 'Oops! There was a problem submitting your form';
+  }
+};
+
+function init() {
+  console.log(
+    `        
+     _ _
+    ( ' )
+   (_ o _)
+    (_,_)  
+    
+    **   **     ******
+    **   **       **  
+    *******       **  
+    *******       **  
+    **   **       **  
+    **   **     ****** ,
+
+    Nice to meet you :)
+    You can check out the source code at - https://github.com/sakshi-011/portfolio-website
+    If you want to connect with me, shoot me an email at - sakshi.singhania011@gmail.com
+
+    `
+  );
+  mainView.addHandlerBio(controlHandlerBio);
+  contact.addHandlerForm(controlHandlerForm);
+}
+init();
