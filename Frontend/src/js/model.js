adcffcd760f267a0880c6f297ab8c1d1ec7ac5c7
sakshi-011 <101360946+sakshi-011@users.noreply.github.com>
2022-06-24 23:56:25 +0530

    Initial Commit

---
+++ Frontend/src/js/model.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//Static data
+export const bioArray = [
+  'Sakshi is a hard working human fuelled by coffee.',
+  'Sakshi is an adaptive and fast learning developer, who takes responsibility for her work and delivers on time.',
+  `Sakshi has worked in a FinTech company as a software engineer where she constantly got good feedback. She was quick to learn the tricks of the trade and gained recognition for writing clean code, good implementation logic, excellent communication skills and integrity. She has a master’s degree in computer applications.`,
+  `Sakshi has worked in a FinTech company as a software engineer. Within 6 months of joining as a fresher, she was deemed capable of directly handling client requests because of excellent communication skills, logical approach, domain knowledge and reliability. She has worked on over 12 modules individually and collaborated with different teams on over 45 modules. She has a master’s degree in computer applications. She is also a coffee and dessert enthusiast.`,
+  `Sakshi is a full stack developer who loves to code (reason she gives to torture herself with challenging problems). Diligently works towards creating efficient solutions which adhere to coding guidelines and the code is readable, maintainable and clean. She has a master’s degree in computer applications (because she is a big time computer nerd). She has good communication skills (just talks about things pertaining to work, doesn’t like to gossip, what a bore!) and takes full responsibility for her work (because her brain is hardwired that way). She is an avid reader (wastes her free time reading books). Gets excited about new projects and believes in continuous learning (Told ya! It's one strange human…)`,
+];
