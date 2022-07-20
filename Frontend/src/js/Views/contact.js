9aac9d5e87df6f2cee154b58d507177cf0f93f94
sakshi-011 <sakshi.singhania011@gmail.com>
2022-07-20 17:03:18 +0530

    Moved variables inside methods

--- Frontend/src/js/Views/contact.js
+++ Frontend/src/js/Views/contact.js
@@ -1,25 +1,22 @@
 'use strict';
 
 class Contact {
-  _form = document.querySelector('.contact--form');
-  _userName = document.querySelector('#user--name');
-  _userEmail = document.querySelector('#user--email');
-  _userMessage = document.querySelector('#user--message');
-  _btnSend = document.querySelector('#btn--send');
-
   validateFormFields() {
-    console.log();
+    const userName = document.querySelector('#user--name');
+    const userEmail = document.querySelector('#user--email');
+    const userMessage = document.querySelector('#user--message');
     if (
-      this._userName.value.length === 0 ||
-      this._userEmail.value.length === 0 ||
-      this._userMessage.value.length === 0
+      userName.value.length === 0 ||
+      userEmail.value.length === 0 ||
+      userMessage.value.length === 0
     )
       return false;
     else return true;
   }
 
   addHandlerForm(handler) {
-    this._form.addEventListener('submit', function (e) {
+    const form = document.querySelector('.contact--form');
+    form.addEventListener('submit', function (e) {
       e.preventDefault();
       handler(e);
     });
--- Frontend/src/js/Views/mainView.js
+++ Frontend/src/js/Views/mainView.js
@@ -1,36 +1,15 @@
 'use strict';
 
 class MainView {
-  _allBlocks = document.querySelectorAll('.block');
-  _footer = document.querySelector('footer');
-  _bioDotsContainer = document.querySelector('.dots--container');
-  _bioContent = document.querySelector('.bio');
-  _imageQuizAnswer = document.querySelector('.quiz--answer');
-  _btnRefresh = document.querySelector('.btn--refresh');
-  _puzzleArea = document.querySelector('.image--container');
-
-  // _sectionObserver = new IntersectionObserver(
-  //   (entries, observer) => {
-  //     const [en] = entries;
-  //     if (!en.isIntersecting) return;
-  //     en.target.classList.remove('section--hidden', 'footer--hidden');
-  //     if (en.target.classList.contains('contact--block'))
-  //       this._footer.classList.remove('footer--hidden');
-  //     observer.unobserve(en.target);
-  //   },
-  //   {
-  //     root: null,
-  //     threshold: 0,
-  //   }
-  // );
-
   renderBio(bioArray) {
+    const bioContent = document.querySelector('.bio');
     const dotActive = document.querySelector('.dot--active');
-    this._bioContent.textContent = bioArray[dotActive.dataset.slide];
+    bioContent.textContent = bioArray[dotActive.dataset.slide];
   }
 
   addHandlerBio(handler) {
-    this._bioDotsContainer.addEventListener('click', function (e) {
+    const bioDotsContainer = document.querySelector('.dots--container');
+    bioDotsContainer.addEventListener('click', function (e) {
       const dot = e.target.closest('.dots');
       if (!dot) return;
       const _bioDots = document.querySelectorAll('.dots');
@@ -41,32 +20,24 @@ class MainView {
   }
 
   renderQuizResult(isWin) {
+    const imageQuizAnswer = document.querySelector('.quiz--answer');
     if (!isWin) return;
-    this._imageQuizAnswer.innerHTML = `<p class="text--small">That is the correct answer :)</p>`;
+    imageQuizAnswer.innerHTML = `<p class="text--small">That is the correct answer :)</p>`;
   }
 
-  //refresh button click - flush previous images
-  //fix css
   renderPuzzleImage(imageURL) {
+    const puzzleArea = document.querySelector('.image--container');
     const html = `<img class="puzzle--piece" src=${imageURL} alt="a random image from sakshi"/>`;
-    this._puzzleArea.insertAdjacentHTML('beforeend', html);
+    puzzleArea.insertAdjacentHTML('beforeend', html);
   }
 
   addHandlerRefreshButton(handler) {
-    this._btnRefresh.addEventListener('click', function (e) {
+    const btnRefresh = document.querySelector('.btn--refresh');
+    btnRefresh.addEventListener('click', function (e) {
       document.querySelector('.image--container').innerHTML = '';
       handler();
     });
   }
-
-  // init() {
-  // this._allBlocks.forEach(block => {
-  //   this._sectionObserver.observe(block);
-  //   block.classList.add('section--hidden');
-  // });
-  // this._sectionObserver.observe(this._footer);
-  // this._footer.classList.add('footer--hidden');
-  // }
 }
 
 export default new MainView();
