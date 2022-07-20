31312e809439210b969262ea3162eab6d50857af
sakshi-011 <sakshi.singhania011@gmail.com>
2022-07-20 17:04:46 +0530

    Added routes

---
+++ Frontend/src/js/routes.js
@@ -0,0 +1,12 @@
+export const routes = {
+  404: '/templates/404.html',
+  '/': '/templates/index.html',
+  '/about': '/templates/about.html',
+  '/games': '/templates/games.html',
+};
+
+export const pages = Object.freeze({
+  HOME: 'home',
+  ABOUT: 'about',
+  GAMES: 'games',
+});
